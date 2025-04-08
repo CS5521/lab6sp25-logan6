0b43a7d47df305b01bcb59ef82f8a5e526a32754
guentherln11 <neonbrownies1337@gmail.com>
2025-04-08 22:09:45 +0000

    fkc works

---
+++ fkcTest.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+int
+main(int argc, char * argv[])
+{
+   int numForks = fkc();
+   printf(1, "%d\n", numForks);
+   if(fork() == 0)
+   {
+      exit();
+   }
+   if(fork() == 0)
+   {
+      exit();
+   }
+   wait();
+   wait();
+   numForks = fkc();
+   printf(1, "%d\n", numForks);
+   exit();
+}
